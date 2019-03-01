923c750608c04800f4ba6b8758aa380150f8c478
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-01 11:43:37 +0900

    テストデータ作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
-FactoryBot.define do
+# FactoryBot.define do
 
-  factory :user do
-    email                 "kkk@gmail.com"
-    password              "00000000"
-    password_confirmation "00000000"
-  end
+#   factory :user do
+#     email                 "kkk@gmail.com"
+#     password              "00000000"
+#     password_confirmation "00000000"
+#   end
 
-end
+# end

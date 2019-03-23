aa80450c1a7cb53683ccb5c446bc5326b8856a7c
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-23 17:23:51 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    
-    redirect_to new_item_path
+    if @item.save
+      redirect_to new_item_path
+    else
+      render :new
+    end
   end
 
   private
--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
-
 module Mercari
-  class Application < Rails::Application
-    
+  class Application < Rails::Application  
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end  
     config.i18n.default_locale = :ja
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

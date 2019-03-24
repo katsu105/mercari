d891be0079e68462363800d7ff274e02d628f4f9
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-24 11:53:15 +0900

    add-seach-action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def search
+    if params[:l_cat]
+      @m_cat = Category.find(params[:l_cat]).children
+    else
+      @s_cat = Category.find(params[:m_cat]).children
+    end
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
 
   def item_params

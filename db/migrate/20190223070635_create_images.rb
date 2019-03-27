7ca415a748053dabe91b24a8e94c3a8bfa4a9798
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-27 17:16:43 +0900

    null: false追加

--- db/migrate/20190223070635_create_images.rb
+++ db/migrate/20190223070635_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
-      t.references :item, foreign_key: true
+      t.references :item, foreign_key: true, null: false
       t.json :image
     end
   end

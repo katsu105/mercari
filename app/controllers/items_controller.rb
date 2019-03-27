407ce9672ced8b62220fb15163f06ccc653c3451
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-27 15:19:36 +0900

    不要な空白削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,14 +13,9 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      
       redirect_to new_item_path
-      
     else
       render :new
-      
-      
-      
     end
   end
 

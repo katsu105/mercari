d684bf37a55ee9aa4c7804bd9ce5633c35ccdc15
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-27 15:15:44 +0900

    不要な記述削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,9 +13,14 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
+      
       redirect_to new_item_path
+      
     else
       render :new
+      
+      
+      
     end
   end
 

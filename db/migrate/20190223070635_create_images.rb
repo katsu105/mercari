5891eae30fcd976184cc0a897744e1d2a98b7d13
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-28 21:07:24 +0900

    mysql2-gem編集

--- db/migrate/20190223070635_create_images.rb
+++ db/migrate/20190223070635_create_images.rb
@@ -1,8 +1,9 @@
 class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
-      t.references :item, foreign_key: true, null: false
+      t.references :item, foreign_key: true
       t.json :image
+      t.timestamps
     end
   end
 end

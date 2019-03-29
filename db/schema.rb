805b8bd74c40266f8ca8837e6cdabcac2c129b9c
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-03-29 22:01:42 +0900

    migrationファイル

--- db/schema.rb
+++ db/schema.rb
@@ -35,16 +35,17 @@ ActiveRecord::Schema.define(version: 20190303032135) do
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                        null: false
-    t.text     "text",          limit: 65535, null: false
+    t.string   "name",                          null: false
+    t.text     "text",            limit: 65535, null: false
     t.string   "price"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "status",                      null: false
-    t.integer  "shipping_cost",               null: false
-    t.integer  "prefectures",                 null: false
-    t.integer  "ship_date",                   null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.integer  "status",                        null: false
+    t.integer  "shipping_cost",                 null: false
+    t.string   "prefectures",                   null: false
+    t.integer  "ship_date",                     null: false
     t.integer  "category_id"
+    t.string   "shipping_method"
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
